684a6caecb1171d6ceaa3eb550f65fa6a840a314
Peter Fleming <fleming_peter01@student.smc.edu>
2025-07-07 21:49:57 -0700

    Added additional test.

--- hobbyTracker.js
+++ hobbyTracker.js
@@ -42,3 +42,5 @@ console.log("Total time spent:", totalTime(hobbyLog), "minutes");
 console.log("Unique hobbies:", uniqueHobbies(hobbyLog));
 console.log("Sessions longer than 30 min:", longSessions(hobbyLog, 30));
 console.log("Number of relaxed sessions:", countMood(hobbyLog, "relaxed"));
+
+console.log("Number of excited sessions:", countMood(hobbyLog, "excited"));
